7c1cbaa440e14f2faa21ccf5983a3a2c3448973a
Swayamhp <swayamgd0@gmail.com>
2024-08-18 13:05:59 +0530

    addedUsernameINposts

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export default function PostCard({post}) {
+  const [ user, setUser ] = useState({});
+
+  useEffect( () =>{
+    const getUser = async () => {
+      try{
+const res = await fetch(`/api/user/${post.userId}`);
+const data = await res.json();
+if(res.ok){
+  setUser(data);
+}
+      }catch(error){
+        console.log(error.message);
+      }
+    }
+    getUser();
+  },[post]);
+  
   return (
     <div className="group relative w-full border h-[400px]
      overflow-hidden hover:border-2 transition-all
@@ -13,7 +30,9 @@ export default function PostCard({post}) {
       </Link>
       <div className='p-3 flex flex-col gap-2 border border-teal-500 '>
         <p className="text-lg font-semibold line-clamp-2">{post.title}</p>
-        <span className="italic text-sm">{post.category}</span>
+        <div className='flex flex-wrap justify-between '>   <span className="italic text-sm">{post.category}</span>
+        <span className="italic text-sm">Post by : {user.username}</span></div>
+     
         <Link to={`/post/${post.slug}`}
         className="z-10 group-hover:bottom-0 absolute bottom-[-200px]
         left-0 right-0 border
